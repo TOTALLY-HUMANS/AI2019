02556a24b53d36356ae22c2c60f4b6361e399763
Ville Talonpoika <ville@talonpoika.net>
2019-10-09 14:56:29 +0300

    AI state machine

--- computer/WebcamClient.py
+++ computer/WebcamClient.py
@@ -8,6 +8,8 @@ import av
 import numpy as np
 from cv2 import aruco
 
+from enum import Enum
+
 from av_video_capture import AvVideoCapture
 from video_capture import VideoCapture
 
@@ -31,9 +33,18 @@ radius_range = [13, 16]
 url = "udp://224.0.0.0:1234"
 # print(cv2.getBuildInformation())
 
+# ROBOT STATE DEFINITIONS
+class RobotState(Enum):
+    Idle = 0
+    ChaseClosestRedBall = 1
+    ChaseClosestGreenBall = 2
+robot_1_state = RobotState.ChaseClosestGreenBall
+robot_2_state = RobotState.ChaseClosestRedBall
 
+# MAIN LOOPERO
 def main():
-    #SI = socketInterface()
+    SI15 = socketInterface()
+    SI16 = socketInterface()
 
     print("Connecting to camera")
     #cap = AvVideoCapture(url)
@@ -67,7 +78,6 @@ def main():
                 tracked = tracker.update(balls)
                 print(tracked)
 
-
                 corners, ids = aruco_detector.get_arucos(img)
                 positions = aruco_detector.get_positions(corners, ids)
                 if len(tracked) >= 3:
@@ -75,58 +85,10 @@ def main():
                     cv2.imwrite('wtf.jpg',img)
                     break
 
-                
-                '''
+                # Run robot AI
+                evaluateRobotState(15, tracked, positions)
+                evaluateRobotState(16, tracked, positions)
 
-
-
-                #for i in ids:
-                    #print("id: ", str(i))
-                robot_pose = (0.0, 0.0, 0.0)
-                for pos in positions:
-
-                    if pos[3] == 16:
-                        robot_pose = pos
-                    ball_found = False
-                    if len(balls) > 0:
-                        ball_pose = balls[0]
-                        ball_x = ball_pose[0]
-                        ball_y = ball_pose[1]
-                        ball_found = True
-
-                    robot_x = robot_pose[0]
-                    robot_y = robot_pose[1]
-                    robot_yaw = robot_pose[2]
-
-                    # ohjauskomento
-                    r_com = 0.0
-                    l_com = 0.0
-                    deadzone = 90
-                    brakezone = 50
-                    if ball_found:
-                        r_com, l_com = drive_commands(
-                            ball_x, ball_y, robot_x, robot_y, robot_yaw)
-                        r_com = 150*r_com #255*r_com
-                        l_com = 150*l_com #255*l_com
-
-                        if abs(r_com) < brakezone:
-                            r_com = 0
-                        elif abs(r_com) < deadzone:
-                            if r_com < 0:
-                                r_com = r_com - deadzone
-                            if r_com > 0:
-                                r_com = r_com + deadzone
-                        if abs(l_com) < brakezone:
-                            l_com = 0
-                        elif abs(l_com) < deadzone:
-                            if l_com < 0:
-                                l_com = l_com - deadzone
-                            if r_com > 0:
-                                l_com = l_com + deadzone
-
-                    # ohjauskomento sokettiin
-                    SI.send_command(r_com, l_com)
-                    '''
             time2 = time.time()
             frame_time = (time2-time1)*1000
             if frame_time != 0:
@@ -169,6 +131,87 @@ def visualize_detected(img, balls, aruco_corners, aruco_ids, positions):
         cv2.line(img, center, forward, color, 2)
     cv2.imshow('Detected', img)
     key = cv2.waitKey(1)
+
+### HERE BEGINS ROBOT AI ###
+
+def evaluateRobotState(robot, ball_positions, robot_positions):
+    if robot == 15:
+        currentRobotState = robot_1_state
+        currentRobotSocket = SI15
+    if robot == 16:
+        currentRobotState = robot_2_state
+        currentRobotSocket = SI16
+    robotStates = {
+        0: Idle,
+        1: ChaseClosestRedBall,
+        2: ChaseClosestGreenBall,
+    }
+    robotStates[currentRobotState](robot, tracked, positions, socket)
+
+def updateState(robot, newState):
+    if robot == 15:
+        robot_1_state = newState
+    if robot == 16:
+        robot_2_state = newState
+
+def Idle(robot, tracked, positions, socket):
+    print("Idling")
+    updateState(robot, RobotState.ChaseClosestGreenBall)
+
+def ChaseClosestRedBall(robot, tracked, positions, socket):
+    print("Chasing red balls...")
+    robot_pose = (0.0, 0.0, 0.0)
+    pose_found = False
+    for pos in positions:
+        if pos[3] == robot:
+            robot_pose = pos
+            pose_found = True
+    if not pose_found:
+        print("No ball found")
+        return
+
+    ball_found = False
+    if len(tracked) > 0:
+        ball_pose = tracked[0]
+        ball_x = ball_pose[0]
+        ball_y = ball_pose[1]
+        ball_found = True
+
+    robot_x = robot_pose[0]
+    robot_y = robot_pose[1]
+    robot_yaw = robot_pose[2]
+
+    # ohjauskomento
+    r_com = 0.0
+    l_com = 0.0
+    deadzone = 90
+    brakezone = 50
+    if ball_found:
+        r_com, l_com = drive_commands(
+            ball_x, ball_y, robot_x, robot_y, robot_yaw)
+        r_com = 150*r_com #255*r_com
+        l_com = 150*l_com #255*l_com
+
+        if abs(r_com) < brakezone:
+            r_com = 0
+        elif abs(r_com) < deadzone:
+            if r_com < 0:
+                r_com = r_com - deadzone
+            if r_com > 0:
+                r_com = r_com + deadzone
+        if abs(l_com) < brakezone:
+            l_com = 0
+        elif abs(l_com) < deadzone:
+            if l_com < 0:
+                l_com = l_com - deadzone
+            if r_com > 0:
+                l_com = l_com + deadzone
+
+    # ohjauskomento sokettiin
+    socket.send_command(r_com, l_com)
+
+def ChaseClosestGreenBall(robot, tracked, positions, socket):
+    print("Chasing green balls...")
 
 
 if __name__ == "__main__":
